d7399c5dc50df48fb912e99e5f8bc5412009b203
Ajikk69 <41413928+Ajikk69@users.noreply.github.com>
2018-09-19 06:53:08 -0700

    Create J1KK.py

---
+++ J1KK.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+cl = LINE("EwUWa7B5E2rXJsJicBF5.rbkBDbQlz2VolkgUiXbrrq.xKInyQzQpXiJOC8r9dE0np7DmDrJpPxfVy284kjaC9WUjl/1Z4zTPFRC10xW4Akz.aaaaza',appName='BIZIOS	1.7.5	iOS	10.2")
+
+
+admin = "u3a30e49a53a2ab0e746546a1b8340c0a"
